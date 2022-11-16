3b3fe9ee41d0bb39ae5222c4ccb8692e4fa0e3b9
laureanomarenco <laureanomarenco@gmail.com>
2022-11-16 17:26:18 -0300

    palindrome-basic

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,3 +1,15 @@
-export const palindromeChecker = (p) => {
+export default function palindromeChecker(input){
+    if(typeof input !== 'string') throw new Error('The argument passed is not a string')
+    let lowerCase = input.toLowerCase()
+    var invertedArray = []
     
+    for(let i = lowerCase.length-1 ; i >= 0 ; i--){
+        let aux = lowerCase[i]
+        invertedArray.push(aux)
+    }
+
+    var inverted = invertedArray.join('')
+
+    if(lowerCase === inverted) return true
+    else return false
 }
--- test/palindromeChecker.test.js
+++ test/palindromeChecker.test.js
@@ -1,5 +1,20 @@
-import { describe } from "vitest";
+import { describe, expect, it } from "vitest";
+import palindromeChecker from "../palindromeChecker.js";
 
 describe('palindrome checker', () => {
-
+    it('should be a function', () => {
+        expect(typeof palindromeChecker).toBe('function')
+    })
+    it('should throw if not string provided as parameter', () => {
+        expect(() => palindromeChecker(14)).toThrow()
+    })
+    it('should always return false if the input its not a palindrome', () => {
+        expect(palindromeChecker('ABC')).toBe(false)
+    })
+    it('should always return true if the input its a palindrome', () => {
+        expect(palindromeChecker('ABBA')).toBe(true)
+    })
+    it('should return true even if the input contains special characters', () => {
+        expect(palindromeChecker('ABB_A')).toBe(true)
+    })
 })
