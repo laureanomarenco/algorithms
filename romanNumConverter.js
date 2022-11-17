35413c9e3c96e5ac7140ec85bdce6ae2cf8697cb
laureanomarenco <laureanomarenco@gmail.com>
2022-11-17 16:59:20 -0300

    romanNumConverted

--- romanNumConverter.js
+++ romanNumConverter.js
@@ -1,3 +1,66 @@
-export default function romanNumConverter(num){
-    
+export default function romanNumConverter(num) {
+  if (typeof num !== "number") throw new Error("The argument is not a number");
+  if (num > 3999)
+    throw new Error("Sorry, only can convert numbers minors of 4000");
+
+  let roman = "";
+
+  let stringNums = num.toString().split("");
+  let nums = stringNums.map((n) => Number.parseInt(n));
+
+  const itsFourOrNine = (n) => {
+    if (n == 4 || n == 9) return true;
+    else return false;
+  };
+
+  let romanCharacter = [
+    {
+      unity: "I",
+      nine: "IX",
+      five: "V",
+      four: "IV",
+    },
+    {
+      unity: "X",
+      nine: "XC",
+      five: "L",
+      four: "XL",
+    },
+    {
+      unity: "C",
+      nine: "CM",
+      five: "D",
+      four: "CD",
+    },
+    {
+      unity: "M",
+    },
+  ];
+
+  let chars = nums.length - 1;
+  for (let j = 0; j <= nums.length - 1; j++) {
+    if (nums[j] < 5) {
+      if (itsFourOrNine(nums[j])) roman = roman + romanCharacter[chars].four;
+      else {
+        for (let i = 0; i < nums[j]; i++) {
+          roman = roman + romanCharacter[chars].unity;
+        }
+      }
+    } else {
+      if (itsFourOrNine(nums[j])) roman = roman + romanCharacter[chars].nine;
+      else {
+        let aux = romanCharacter[chars].five;
+        for (let i = 5; i < nums[j]; i++) {
+          if (nums[j] === 5) return;
+          else {
+            aux = aux + romanCharacter[chars].unity;
+          }
+        }
+        roman = roman + aux;
+      }
+    }
+    chars = chars - 1;
+  }
+
+  return roman;
 }
--- test/romanNumConverter.test.js
+++ test/romanNumConverter.test.js
@@ -5,4 +5,32 @@ describe('roman numeral converter', () => {
     it('should be a function', () => {
         expect(typeof romanNumConverter).toBe('function')
     })
+    it('should throw an error if the input its not a number', () => {
+        expect(() => romanNumConverter('string')).toThrow()
+    })
+    it('should throw an error if the input its mayor to 3999', () => {
+        expect(() => romanNumConverter(4000)).toThrow()
+    })
+    it('should return the number passed in roman form', () => {
+        
+        expect(romanNumConverter(3)).toBe('III')
+        expect(romanNumConverter(4)).toBe('IV')
+        expect(romanNumConverter(5)).toBe('V')
+        expect(romanNumConverter(9)).toBe('IX')
+        expect(romanNumConverter(8)).toBe('VIII')
+        expect(romanNumConverter(83)).toBe('LXXXIII')
+        expect(romanNumConverter(29)).toBe('XXIX')
+        expect(romanNumConverter(649)).toBe('DCXLIX')
+        expect(romanNumConverter(798)).toBe('DCCXCVIII')
+        expect(romanNumConverter(44)).toBe('XLIV')
+        expect(romanNumConverter(3999)).toBe('MMMCMXCIX')
+        expect(romanNumConverter(1004)).toBe('MIV')
+
+        
+
+
+
+
+        
+    })
 })
